c4453de5a12d6d8ed762d1384cce17d3f364a978
Rostislav Simonik <rostislav.simonik@technologystudio.sk>
2021-04-03 15:46:48 +0200

    Add missing exports

---
+++ packages/@txo/service-react/src/index.ts
@@ -0,0 +1,10 @@
+/**
+ * @Author: Rostislav Simonik <rostislav.simonik@technologystudio.sk>
+ * @Date: 2021-04-03T12:04:14+02:00
+ * @Copyright: Technology Studio
+**/
+
+export * from './Hooks/Service'
+export * from './Redux/Services'
+export * from './Sagas'
+export * from './Model/Types'
