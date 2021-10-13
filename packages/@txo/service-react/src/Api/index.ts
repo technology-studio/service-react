63d836ff48afdda718a08365f61cb44dde13bd30
Erik Slovak <erik.slovak@technologystudio.sk>
2021-10-13 19:53:02 +0200

    create service context helper function and export it

---
+++ packages/@txo/service-react/src/Api/index.ts
@@ -0,0 +1,8 @@
+/**
+ * @Author: Erik Slovak <erik.slovak@technologystudio.sk>
+ * @Date: 2021-10-13T19:10:03+02:00
+ * @Copyright: Technology Studio
+**/
+
+export * from './ContextHelper'
+export * from './EvaulatedValueHelper'
